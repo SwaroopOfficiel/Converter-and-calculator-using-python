d1c5926ce52cfcc05b7ffafc6e31d2d7992d67dc
Chaitanya <VankarChaitanya@outlook.com>
2020-09-01 13:24:59 +0530

    Add files via upload
    This is a converter which converts kilometre to miles

---
+++ Convert kilometer into miles.py
@@ -0,0 +1,8 @@
+kilometer_kg = float(input("Enter the number:"))
+convert = 1.6 * kilometer_kg
+print(convert)
+if convert < 1.6:
+    print(kilometer_kg)
+    print("This is less than a kilometer")
+else:
+    print("This is more than a kilometer")
