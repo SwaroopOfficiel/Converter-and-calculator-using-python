a33d0b5855c713643831f43825f9a9539b09bc4b
Chaitanya <VankarChaitanya@outlook.com>
2020-09-07 22:53:10 +0530

    Add files via upload
    This is meter to centimetre and centimetre to meter convert using python code

---
+++ Meter conversation.py
@@ -0,0 +1,9 @@
+# cenitmeter to meter 
+cenitmeter_cm = float(input('Enter the Number: '))
+meter_m = (cenitmeter_cm/100)
+print(meter_m)
+
+# meter to cenitmeter 
+meter_m=float(input('Enter the Number: '))
+cenitmeter_cm = meter_m * 100
+print(cenitmeter_cm)
